9684d13ac3b12a449a1de7b3da4a55067f47a3f9
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2022-08-06 00:23:16 +0700

    add arduino folder and esp8266 folder

---
+++ esp8266/RSAAuth.cpp
@@ -0,0 +1,139 @@
+/*
+  DateTime.cpp - Arduino Date and Time library
+  Copyright (c) Michael Margolis.  All right reserved.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+extern "C" {
+  // AVR LibC Includes
+}
+
+#include "RSAAuth.h"
+
+
+SoftwareSerial swSer(4, 5, false, 256); //D2, D1 = SRX, STX
+RSAAuth::RSAAuth()
+{
+
+}
+
+
+byte RSAAuth::GenCrc(byte en[], int n)
+{
+  long tong = 0;
+  for (int i = 0; i < n; i++)
+  {
+    tong += en[i];
+    tong %= 253;
+  }
+  return (byte)tong;
+}
+int RSAAuth::Package(byte en[], int n, byte pac[])
+{
+  int i;
+  byte crc = GenCrc(en, n);
+  pac[0] = 255;
+  for (i = 0; i < n; i++)
+  {
+    pac[i + 1] = (byte)en[i];
+  }
+  pac[i + 1] = crc;
+  pac[i + 2] = 254;
+  return n + 3;
+}
+int RSAAuth::OpenPackage(byte pac[], int m, byte en[])
+{
+  if (pac[0] != 255 || pac[m - 1] != 254) return -1;
+  byte crc = GenCrc(pac + 1, m - 3);
+  if (crc != pac[m - 2]) return -1;
+  for (int i = 1; i < m - 2; i++)
+    en[i - 1] = pac[i];
+  return m - 3;
+}
+bool RSAAuth::checkArray(byte arr[], int*BEGIN, int*END)
+{
+  bool dau = false, cuoi = false;
+  for (int i = *BEGIN; i != *END; i++, i %= 255)
+  {
+
+    if (arr[i] == 255) dau = true;
+    if (arr[i] == 254 && dau == true) return true;
+  }
+  return false;
+}
+bool RSAAuth::CutString(byte arr[], int*BEGIN, int*END, byte desArr[], int *pDesLen)
+{
+
+  if (checkArray(arr, BEGIN, END) == true)
+  {
+    //Serial.println("Nhan:");
+    //Serial.println(*BEGIN);
+    //Serial.println(*END);
+    while (arr[*BEGIN] != 255) //start
+    {
+      (*BEGIN)++;
+      (*BEGIN) %= 255;
+    }
+
+    *pDesLen = 0;
+    while (arr[*BEGIN] != 254) //end
+    {
+      desArr[(*pDesLen)++] = arr[(*BEGIN)++];
+      (*BEGIN) %= 255;
+    }
+
+    desArr[(*pDesLen)++] = arr[(*BEGIN)++];
+    (*BEGIN) %= 255;
+    return true;
+  }
+  return false;
+}
+void RSAAuth::Send(char cstr[])
+{
+
+
+  int len;//=encrypt(cstr,en);
+  for (len = 0; cstr[len] != 0; len++)
+    en[len] = cstr[len];
+  en[len] = 0;
+  len = Package(en, len, pac);
+  int i = 0;
+  while (i < len) {
+    swSer.write(pac[i++]);
+    yield();
+  }
+
+}
+
+bool RSAAuth::Receive(byte buff[], int*BEGIN, int*END, byte m[], int *pDesLen)
+{
+  int i;
+  while (swSer.available() > 0 && (*END + 1) % 255 != *BEGIN) {
+    buff[(*END)++] = swSer.read();
+    (*END) %= 255;
+    yield();
+  }
+
+  if (CutString(buff, BEGIN, END, desArr, pDesLen))
+  {
+    Serial.print("Nhan :");
+    //    Serial.println((char*)buff);
+    *pDesLen = OpenPackage(desArr, *pDesLen, unpac);
+    if (*pDesLen >= 0)
+    {
+      desArr[*pDesLen] = 0;
+      //rsa.decrypt(unpac,m,*pDesLen);
+      for (i = 0; i <= *pDesLen; i++)
+        m[i] = unpac[i];
+      m[*pDesLen] = 0;
+      //Serial.println("Nhan:");
+      //Serial.println((char*)m);
+      return true;
+    }
+    else return false;//Serial.println("Nhan error:");
+  }
+  return true;
+}
+RSAAuth rsa = RSAAuth() ;
---
+++ esp8266/esp8266.ino
@@ -0,0 +1,142 @@
+#include "RSAAuth.h"                         // Khai báo sử dụng thư viện RSA (Rx = D1, Tx = D2)
+#include <ArduinoJson.h>                     // Khai báo sử dụng thư viện đọc chuỗi Json
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+//116.118.48.230
+//192.168.43.32
+// Thông tin về wifi
+#define ssid "Hai Anh"
+#define password "123456789"
+#define mqtt_server "116.118.48.230"
+#define mqtt_user "doan2"         // Tài khoản đăng nhập broker
+#define mqtt_pwd  "doan2"    // Mật khẩu đăng nhập broker
+const uint16_t mqtt_port = 1883; //Port của CloudMQTT TCP
+
+#define BAUD_RATE 115200                       // Tốc độ giao tiếp UART BAUD_RATE bit/s
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+DynamicJsonDocument doc(2048); // Khởi tạo document Jon động với dữ liệu được lưu trong bộ nhớ heap
+// trong đề tài này chúng em sử dụng 2kb <=> 2028 byte
+// khuyến khích document có kích thước lớn hơn 1kb
+
+/////______Các biến cho việc gửi và nhận______/////
+char  cstr[255];
+byte  m[255];
+byte  buff[255];
+int   BEGIN = 0;
+int   END = 0;
+int   DesLen = 0;
+
+/////______Các biến cho việc gửi và nhận______/////
+// Hàm kết nối wifi
+
+void setup_wifi();
+void handle(String payload);
+void callback(char* topic, byte* payload, unsigned int length);
+void reconnect();
+void receive_data_from_arduino_mega();
+
+void setup()
+{
+  Serial.begin(BAUD_RATE);    // Tốc độ giao tiếp với máy tính
+  swSer.begin(BAUD_RATE);     // Tốc độ giao tiếp UART giữa 2 esp BAUD_RATE bit/s
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop()
+{
+  if (!client.connected())// Kiểm tra kết nối
+    reconnect();
+  client.loop();
+  receive_data_from_arduino_mega();
+}
+
+void setup_wifi()
+{
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+// Hàm call back để nhận dữ liệu
+// Hàm call back để nhận dữ liệu
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  for (int i = 0; i < length; i++) {
+    cstr[i] = 0;
+    cstr[i] = (char)payload[i];
+  }
+  yield();                                        // Delay một khoản thời gian rất ngắn
+  rsa.Send(cstr);                                 // Gửi chuỗi cstr qua device
+}
+
+// Hàm reconnect thực hiện kết nối lại khi mất kết nối với MQTT Broker
+void reconnect()
+{
+  while (!client.connected()) // Chờ tới khi kết nối
+  {
+    // Thực hiện kết nối với mqtt user và pass
+    if (client.connect("ESP8266Client", mqtt_user, mqtt_pwd)) //kết nối vào broker
+    {
+      Serial.println("Đã kết nối:");
+      client.subscribe("doan2/onOff/led"); //đăng kí nhận dữ liệu từ topic IoT47_MQTT_Test
+      client.subscribe("doan2/getState");
+    }
+    else
+    {
+      Serial.print("Lỗi:, rc=");
+      Serial.print(client.state());
+      Serial.println("Thu lai sau 5s");
+      // Đợi 5s
+      delay(5000);
+    }
+  }
+}
+
+void handle(String payload) {
+  deserializeJson(doc, payload);         // Khởi tạo một document có kích thước động là 2kb để phân tích chuỗi Json truyền vào
+  JsonObject obj = doc.as<JsonObject>(); // Tạo 1 obj có kiểu dữ liệu là JsonObject với document vừa tạo
+  byte idtp = obj["idtp"];                // ID Topic
+  byte idc = obj["idc"];                // ID connect
+  byte idgw = obj["id"];                // ID getway
+  if (idc == 1 && idgw == 1) {
+    switch (idtp) {
+      case 1:
+        client.publish("doan2/status", (char*)m); // gửi dữ liệu lên topic doan2/status
+        break;
+      case 2:
+        client.publish("doan2/onOff/led/feedback", (char*)m); // gửi dữ liệu phản hồi điều khiển nút nhấn lên topic doan2/status/led
+        break;
+      default: break;
+    }
+  }
+}
+
+void receive_data_from_arduino_mega() {
+  DesLen = 0;
+  if (rsa.Receive(buff, &BEGIN, &END, m, &DesLen)) // Nếu có dữ liệu từ device truyền qua
+    // Nó sẽ kiểm tra dữ liệu nhận được đúng với dữ liệu mà device truyền qua
+    // thì thực hiện pub lên broker
+  {
+    if (DesLen > 0)
+    {
+      handle((char*)m);
+      Serial.println((char*)m);
+    }
+  }
+  else Serial.println("Nhan error:");
+}
